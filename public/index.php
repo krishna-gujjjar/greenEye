27afe94f55a61479184564b98052310596c2009b
Krishna Gujjjar <krishnagujjjar@gmail.com>
2019-03-08 00:53:25 +0530

    Create Entry Point

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php ini_set('display_errors', 1); // Show Errors
+/**
+ * Intry Point of File
+ *
+ * User's Entry Point For Requested Methods & Pages
+ *
+ * @package				greenEye
+ * @author				Krishna Gujjjar <krishnagujjjar@gmail.com>
+ *
+ * @Date:				2019-03-08 00:50:58
+ * @Last Modified by: krishna_gujjjar
+ * @Last Modified time: 2019-03-08 00:53:11
+ */
