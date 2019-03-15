b221e1fd2709f17526b117f852387dd1aeb0f47c
Krishna Gujjjar <krishnagujjjar@gmail.com>
2019-03-15 13:33:28 +0530

    Inc Files

---
+++ app/includes/footer.php
@@ -0,0 +1,125 @@
+<!-- ***** Footer Area Start ***** -->
+<footer class="footer-area section-padding-100">
+    <!-- Main Footer Area -->
+    <div class="main-footer-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-xl-3">
+                    <div class="footer-widget-area">
+                        <div class="footer-logo">
+                            <img src="<?php echo ASSETS; ?>img/core-img/logo.png" alt="" />
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer
+                            adipiscing elit, sed diam nonummy nibh
+                            euismod tincidunt ut laoreet dolore magna
+                            aliquam erat volutpat. Lorem ipsum dolor sit
+                            amet, consectetuer.
+                        </p>
+                        <div class="footer-social-info">
+                            <a href="#"><i class="fill-facebook stroke-none" data-feather="facebook"></i></a>
+                            <a href="#"><i class="fill-github stroke-none" data-feather="github"></i></a>
+                            <a href="#"><i class="fill-slack stroke-none" data-feather="slack"></i></a>
+                            <a href="#"><i class="fill-twitter stroke-none" data-feather="twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Google Maps -->
+                <div class="col-12 col-sm-6 col-xl-3">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d461369.30528190744!2d75.78084025401128!3d25.389820465253212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396f9b30c41bb44d%3A0x5f5c103200045588!2sKota%2C+Rajasthan!5e0!3m2!1sen!2sin!4v1552324077362" width="100%" height="350" frameborder="0"></iframe>
+                </div>
+
+                <div class="col-12 col-sm-6 col-xl-3">
+                    <div class="footer-widget-area">
+                        <div class="widget-title">
+                            <h6>Contact Form</h6>
+                        </div>
+                        <div class="footer-contact-form">
+                            <form action="#" method="post">
+                                <input type="text" class="form-control border-top-0 border-right-0 border-left-0" name="footer-name" id="footer-name" placeholder="Name" />
+                                <input type="email" class="form-control border-top-0 border-right-0 border-left-0" name="footer-email" id="footer-email" placeholder="Email" />
+                                <textarea name="message" class="form-control border-top-0 border-right-0 border-left-0" id="footerMessage" placeholder="Message"></textarea>
+                                <button type="submit" class="btn greeneye-btn">
+                                    Contact Us <span>+</span>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-xl-3">
+                    <div class="footer-widget-area">
+                        <div class="widget-title">
+                            <h6>News Letter</h6>
+                        </div>
+
+                        <div class="footer-newsletter-area">
+                            <form action="#">
+                                <input type="email" class="form-control border-0 mb-0" name="newsletterEmail" id="newsletterEmail" placeholder="Your Email Here" />
+                                <button type="submit">Subscribe</button>
+                            </form>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer
+                                adipiscing elit, sed diam nonummy nibh
+                                euismod tincidunt ut laoreet dolore
+                                magna aliquam erat volutpat. Lorem ipsum
+                                dolor sit amet, consectetuer.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bottom Footer Area -->
+    <div class="bottom-footer-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="bottom-footer-content">
+                        <!-- Copywrite Text -->
+                        <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=ff30347ce9cf225ce7e29e8db05e4660b4a7f794'></script>
+                        <script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/502954/t/0"></script>
+                        <div class="copywrite-text">
+                            <p>
+                                Copyright &copy; <span id="year"></span>
+                                All rights reserved |
+                                made with
+                                <i data-feather="heart" class="fill-danger stroke-none"></i>
+                                by
+                                <a href="#">Gurjar's ProTech</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- ***** Footer Area End ***** -->
+
+<!-- Feather js -->
+<script src="<?php echo VENDORS; ?>feather/feather.min.js"></script>
+<!-- jQuery js-->
+<script src="<?php echo VENDORS; ?>jquery/jquery-2.2.4.min.js"></script>
+<!-- <script src="<?php echo VENDORS; ?>jquery/jquery-3.3.1.min.js"></script> -->
+<!-- Popper js -->
+<script src="<?php echo VENDORS; ?>popperjs/popper.min.js"></script>
+<!-- Bootstrap js -->
+<script src="<?php echo VENDORS; ?>bootstrap/js/bootstrap.min.js"></script>
+<!-- Counter Plugin js -->
+<script src="<?php echo VENDORS; ?>counterup/plugins.js"></script>
+<!-- Active js -->
+<script src="<?php echo ASSETS; ?>js/main.js"></script>
+<script>
+    // Get Current Year for Copyright
+    $('#year').html(new Date().getFullYear());
+    // Feather Icons
+    feather.replace({
+        'fill': "currentColor",
+    });
+</script>
+</body>
+
+</html>
---
+++ app/includes/header.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Title  -->
+    <title>GreenEye - Health &amp; Medical | Home</title>
+
+    <!-- Favicon  -->
+    <link rel="icon" href="<?php echo ASSETS; ?>img/core-img/favicon.ico" />
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="<?php echo VENDORS; ?>bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo VENDORS; ?>animate/animate.css">
+    <link rel="stylesheet" href="<?php echo ASSETS; ?>css/owl.carousel.css">
+    <link rel="stylesheet" href="<?php echo ASSETS; ?>css/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo ASSETS; ?>css/themify-icons.css">
+    <link rel="stylesheet" href="<?php echo ASSETS; ?>css/greeneye-icons.css">
+    <link rel="stylesheet" href="<?php echo ASSETS; ?>css/nice-select.css">
+    <link rel="stylesheet" href="<?php echo ASSETS; ?>style/main.min.css">
+    <!-- <link rel="stylesheet" href="assets/css/main.css" /> -->
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div id="preloader">
+        <img src="<?php echo ASSETS; ?>img/core-img/preloader.svg" class="centered" alt="">
+    </div>
+
+
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area">
+        <!-- Top Header Area -->
+        <div class="top-header-area">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100">
+                        <div class="h-100 d-md-flex justify-content-between align-items-center">
+                            <p>Welcome to <span>GreenEye</span></p>
+                            <p>
+                                Opening Hours : Monday to Saturday - 8am to
+                                10pm Contact : <span>+91-123-456-7890</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Main Header Area -->
+        <div class="main-header-area" id="stickyHeader">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-12 h-100">
+                        <div class="main-menu h-100">
+                            <nav class="navbar h-100 navbar-expand-lg">
+                                <!-- Logo Area  -->
+                                <a class="navbar-brand" href="index">
+                                    <img src="<?php echo ASSETS; ?>img/core-img/logo.png" alt="Logo" />
+                                </a>
+
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#medilifeMenu" aria-controls="medilifeMenu" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+
+                                <div class="collapse navbar-collapse" id="medilifeMenu">
+                                    <!-- Menu Area -->
+                                    <ul class="navbar-nav ml-auto">
+                                        <li class="nav-item active">
+                                            <a class="nav-link" href="index">Home
+                                                <span class="sr-only">(current)</span>
+                                            </a>
+                                        </li>
+
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="about-us">About Us</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="services">Services</a>
+                                        </li>
+                                        <!-- <li class="nav-item">
+											<a class="nav-link" href="blog">News</a>
+										</li> -->
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="contact">Contact</a>
+                                        </li>
+                                    </ul>
+                                    <!-- Appointment Button -->
+                                    <a href="#" class="btn greeneye-appoint-btn ml-30">For <span>emergencies</span> Click
+                                        here</a>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
