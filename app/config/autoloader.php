3f7bb7d26d67fb05846ccbb7f1fd71a24252b38b
Krishna Gujjjar <krishnagujjjar@gmail.com>
2019-03-28 23:36:57 +0530

    Fix Path of Autoload Classes For All Type OS

--- app/config/autoloader.php
+++ app/config/autoloader.php
@@ -3,7 +3,7 @@
 /** @param string $className */
 spl_autoload_register(function ($className) {
     /** @var string|false Required Class's Path */
-    $path = realpath(str_replace('greeneye/', ROOT, strtolower(str_replace(BS, DS, $className))) . '.php');
+    $path = realpath(str_replace('greeneye/', ROOT, strtolower(str_replace('\\', DIRECTORY_SEPARATOR, $className))) . '.php');
     if (file_exists($path)) {
         return require_once $path;
     }
