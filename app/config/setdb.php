96606c52ad255eabe7c91dcbc28c050e215fd384
Krishna Gujjjar <krishnagujjjar@gmail.com>
2019-03-28 23:45:35 +0530

    Set database Constants

---
+++ app/config/setdb.php
@@ -0,0 +1,31 @@
+<?php namespace GreenEye\App\Config;
+
+/** Database Type Name
+ *
+ * Type of Our Database
+ * * By Default It's MySql */
+defined('DB_TYPE') or define('DB_TYPE', 'mysql');
+
+/** Database Host Name
+ *
+ * Name of Our Database Hostname
+ * * By Default It's LocalHost */
+defined('DB_HOST') or define('DB_HOST', 'localhost');
+
+/** Database User Name
+ *
+ * Name of Our Database User
+ * * By Default It's Root */
+defined('DB_USER') or define('DB_USER', 'root');
+
+/** Database User Password
+ *
+ * Name of Our Database User's Password
+ * * By Default It's Root */
+defined('DB_PASS') or define('DB_PASS', 'root');
+
+/** Database Name
+ *
+ * Name of Our Database Name
+ * * By Default It's gReeneye */
+defined('DB_NAME') or define('DB_NAME', 'gReeneye');
