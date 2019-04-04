3bcc843d0235f1cb4400ab9ed2708321ff77929f
Krishna Gujjjar <krishnagujjjar@gmail.com>
2019-04-04 22:30:53 +0530

    Error Pages

---
+++ public/error/401.php
@@ -0,0 +1,10 @@
+<?php require_once '../../__constants.php'; ?>
+<?php use GreenEye\App\Config\Import; ?>
+<?php Import::Header(); ?>
+<div class="container py-5">
+    <div class="col pt-5">
+        <h1 class="font-bold text-center mt-md-5 pt-5">I Think You Are Lost, Your Page Not Found.</h1>
+        <img class="bg-img pt-3" src="<?php echo ASSETS; ?>img/svg/dreamer.svg" alt="">
+    </div>
+</div>
+<?php Import::Footer(); ?>
