279540cfb3b879b8cbc9ecd84103cb6652c13032
Krishna Gujjjar <krishnagujjjar@gmail.com>
2019-04-06 12:08:18 +0530

    Create `gReeneye_dOctor` Table

--- app/helper/db.php
+++ app/helper/db.php
@@ -20,25 +20,30 @@ abstract class DB
                 $selectDB = mysqli_select_db($this->connect, DB_NAME) or die(errMsg('Oops, Database Not Selected.'));
                 if ($selectDB) {
                     $createUser = "CREATE TABLE IF NOT EXISTS `gReeneye_uSer` (
-                            `gReeneye_uiD` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Admin ID',
-                            `gReeneye_unamE` varchar(255) NOT NULL COMMENT 'Admin Name',
-                            `gReeneye_upasS` varchar(255) NOT NULL COMMENT 'Admin Password',
-                            `gReeneye_uriD` int(11) DEFAULT NULL COMMENT 'Who Created ?',
-                            `gReeneye_ucdatE` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Admin''s Create Date',
-                            `gReeneye_ulogiN` json DEFAULT NULL COMMENT 'Admin''s Login Time Details',
-                            `gReeneye_ulvL` int(11) NOT NULL DEFAULT '2' COMMENT 'Admin''s Level',
-                            `gReeneye_upiC` text COMMENT 'Admin Profile Pic'
-                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Admin''s List'";
-                    $createUser = mysqli_query($this->connect, $createUser) or exit(errMsg('Oops, Table Not Created'));
-                    if ($createUser) {
-                        $checkIndex = "SHOW INDEX FROM `gReeneye_uSEr`";
-                        $checkIndex = mysqli_query($this->connect, $checkIndex) or exit(errMsg('Unable to Find Indexes in Database'));
-                        $rowss = mysqli_num_rows($checkIndex);
-                        if ($rowss < 2) {
-                            $indexDB = "ALTER TABLE `gReeneye_uSer`
-                                ADD KEY `rID` (`gReeneye_uriD`)";
-                            $indexDB = mysqli_query($this->connect, $indexDB) or exit(errMsg('Unable to Add Index in Database'));
-                        }
+                                    `gReeneye_uiD` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Admin ID',
+                                    `gReeneye_unamE` varchar(255) NOT NULL COMMENT 'Admin Name',
+                                    `gReeneye_upasS` varchar(255) NOT NULL COMMENT 'Admin Password',
+                                    `gReeneye_uriD` int(11) NOT NULL COMMENT 'Who Created ?',
+                                    `gReeneye_ucdatE` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Admin\'s Create Date',
+                                    `gReeneye_ulogiN` longtext DEFAULT NULL COMMENT 'Admin\'s Login Time Details',
+                                    `gReeneye_ulvL` int(1) NOT NULL DEFAULT '2' COMMENT 'Admin\'s Level',
+                                    `gReeneye_upiC` text COMMENT 'Admin Profile Pic',
+                                    INDEX `rID` (`gReeneye_uriD`)
+                                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Admin\'s List'";
+                    $createUser = mysqli_query($this->connect, $createUser) or exit(errMsg('Oops, `gReeneye_uSer` Table Not Created'));
+                    $createDoctor = "CREATE TABLE IF NOT EXISTS `gReeneye`.`gReeneye_dOctor` (
+                                        `gReeneye_diD` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Doctor\'s ID' ,
+                                        `gReeeneye_dnamE` VARCHAR(255) NOT NULL COMMENT 'Doctor\'s Name' ,
+                                        `gReeneye_dexP` INT(1) NOT NULL DEFAULT '1' COMMENT 'Doctor\'s Experience' ,
+                                        `gReeneye_dspl` VARCHAR(255) NOT NULL COMMENT 'Doctor\'s Speciality' ,
+                                        `gReeneye_dratE` INT(1) NOT NULL DEFAULT '1' COMMENT 'Doctor\'s Rating' ,
+                                        `gReeneye_dpiC` TEXT DEFAULT NULL COMMENT 'Doctor\'s Pic' ,
+                                        `gReeneye_dcrT` INT NOT NULL COMMENT 'Doctor\'s Creator \'s ID' ,
+                                        `gReeneye_dcrtD` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Doctor\'s Create Time' ,
+                                        INDEX `createID` (`gReeneye_dcrT`)
+                                    ) ENGINE = InnoDB DEFAULT CHARSET=utf8 COMMENT = 'Doctor\'s Table'";
+                    $createDoctor = mysqli_query($this->connect, $createDoctor) or exit(errMsg('Oops, `gReeneye_dOctor` Table Not Created'));
+                    if ($createUser && $createDoctor) {
                         $checkData = "SELECT * FROM `gReeneye_uSer` WHERE `gReeneye_uiD` = 1";
                         $checkData = mysqli_query($this->connect, $checkData) or exit(errMsg("Sorry, Can't Find Any Data in Database"));
                         if ($checkData) {
