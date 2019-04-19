592330ed0c550729eda4d5d154a3e60eb8601770
asifkhan000 <ak9113189@gmail.com>
2019-04-19 13:04:25 +0530

    implemented extented table

--- public/admin/index.php
+++ public/admin/index.php
@@ -144,7 +144,61 @@
             </div>
         </div>
     </div>
-    <?php Import::getFooter(); ?>
-<?php else : ?>
-    <?php header('location:' . LOGIN); ?>
-<?php endif; ?>
+
+
+
+    <!-- All appointments -->
+
+
+
+    <div class="row">
+        <div class="col-md-12 col-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-title-wrap bar-info">
+                        <h4 class="card-title">All Appointments</h4>
+                        <?php $Book = new Appointment; ?>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="card-block">
+                        <table id="example" class="display" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Patient's Name</th>
+                                    <th>Mobile Number</th>
+                                    <th>Appointment Date</th>
+                                    <th>Booking Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1; ?>
+                                <?php if ($newBook = $Book->showallAppointment('Book')) : ?>
+                                    <?php foreach ($newBook as $bookedUser) : ?>
+                                        <tr>
+                                            <th scope="row"><?php echo $i++; ?></th>
+                                            <td><?php echo $bookedUser['gReeeneye_bnamE']; ?></td>
+                                            <td>+91 <?php echo $bookedUser['gReeneye_bnuM']; ?></td>
+                                            <td><?php echo $bookedUser['gReeneye_bdatE'] . ' @ ' . $bookedUser['gReeneye_btimE'] ?></td>
+                                            <td><?php echo date('F d, Y' . ' @ ' . ' h : i A', strtotime($bookedUser['gReeneye_bcrT'])); ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </body>
+
+        </html>
+
+
+
+        <?php Import::getFooter(); ?>
+    <?php else : ?>
+        <?php header('location:' . LOGIN); ?>
+    <?php endif; ?>
